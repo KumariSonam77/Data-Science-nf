13447178f220ccefebce0613422de5890284fe83
kuhu77 <kumarisonam08685@gmail.com>
2026-06-06 18:40:02 +0530

    updated double bar chart

--- Data-Science-nf/Module 3/bar.py
+++ Data-Science-nf/Module 3/bar.py
@@ -1,36 +1,17 @@
-#Bar Chart (Double bar)
-
+#dobble bar chart
+import pandas as pd
 import matplotlib.pyplot as plt
 
-subject = ["Python", "JavaScript", "React.js", "TypeScript"]
-score1 = [50, 60, 70, 80]
-score2 = [55, 65, 75, 85]
+df = pd.DataFrame({
+    "Exam1": [50, 60, 70, 80],
+    "Exam2": [55, 65, 75, 85]
+}, index=["Python", "JavaScript", "React.js", "TypeScript"])
 
-# positions 
-x = list(range(len(subject)))
-width = 0.35
+df.plot(kind="bar", figsize=(7,5))
 
-# bars
-plt.bar([i - width/2 for i in x], score1, width, label="Exam 1", color="green")
-plt.bar([i + width/2 for i in x], score2, width, label="Exam 2", color="blue")
-
-# labels
-plt.xlabel("Subjects")
-plt.ylabel("Score")
 plt.title("Double Bar Chart")
-
-# grid
-plt.grid(axis="y", linestyle="--", linewidth=1, color="gray", alpha=0.5)
-
-# x-axis labels
-plt.xticks(x, subject)
-
-# values on bars
-for i in x:
-    plt.text(i - width/2, score1[i] + 1, str(score1[i]), ha="center")
-    plt.text(i + width/2, score2[i] + 1, str(score2[i]), ha="center")
-
-# legend
-plt.legend()
+plt.ylabel("Scores")
+plt.xlabel("Subjects")
+plt.grid(axis="y", linestyle="--", alpha=0.5)
 
 plt.show()
